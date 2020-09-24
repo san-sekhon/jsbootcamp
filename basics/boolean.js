960a494565b2ee019aa923c697e5e57ea2261539
Sandeep Sekhon <sandysss79@gmail.com>
2020-09-23 22:14:38 -0700

    finished boolean-advanced.js

--- basics/boolean.js
+++ basics/boolean.js
@@ -1,5 +1,12 @@
-let age = 41;
+let age = 5;
 let isChild = age <= 7;
 let isSenior = age >= 65;
 console.log(isChild);
 console.log(isSenior);
+
+if(age <= 7) {
+  console.log('child pricing');
+}
+if(age >= 65) {
+  console.log('senior pricing');
+}

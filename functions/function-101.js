89f96c6e6f37f5b64b61a6233400bc8548278d2a
Sandeep Sekhon <sandysss79@gmail.com>
2020-09-28 10:56:15 -0700

    finished functions-101.js

---
+++ functions/function-101.js
@@ -0,0 +1,6 @@
+let convertFahrenheitToCelsius = function(fahrenheit) {
+  return (fahrenheit -32) * 5 / 9;
+};
+
+console.log(convertFahrenheitToCelsius(32));
+console.log(convertFahrenheitToCelsius(212));

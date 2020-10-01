4bdebb8078d9241023e5b90dc9d676080c7d3bcf
Sandeep Sekhon <sandysss79@gmail.com>
2020-10-01 12:20:11 -0700

    finished template strings

--- functions/arguments.js
+++ functions/arguments.js
@@ -1,5 +1,5 @@
 let getTip = function(total, tipPercent = 0.2) {
-   return total * tipPercent;
+   return `A ${tipPercent}% tip on ${total} would be ${total * tipPercent}`;
 }
  let tip = getTip(100, 0.1);
  console.log(tip);

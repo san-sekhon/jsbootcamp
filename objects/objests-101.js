b1bc5863b8f18b02e881589c0bcf5ba69c66c1f3
Sandeep Sekhon <sandysss79@gmail.com>
2020-10-01 21:13:17 -0700

    finished objects-101.js

---
+++ objects/objests-101.js
@@ -0,0 +1,11 @@
+let person = {
+  name: 'Sandeep Sekhon',
+  age: 41,
+  location: 'California'
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`);
+
+person.age += 1;
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`);

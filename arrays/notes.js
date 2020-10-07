fc688cdf6461aecd32f9d6fd2e24d5d81151ebc9
Sandeep Sekhon <sandysss79@gmail.com>
2020-10-06 19:27:27 -0700

    finished var.js, notes.js, todo.js

---
+++ arrays/notes.js
@@ -0,0 +1,12 @@
+const notes = ['Note 1', 'Note 2', 'Note 3'];
+// console.log(notes.pop());
+// notes.push('Note 4');
+
+// console.log(notes.shift());
+// notes.unshift('My first note');
+
+//notes.splice(1, 1, 'This is the new second item' );
+
+notes[2] = 'This is now the new note 3';
+console.log(notes.length);
+console.log(notes);
---
+++ arrays/todo.js
@@ -0,0 +1,18 @@
+const todos = ['study js', 'teach js', 'teach jus', 'do homework', 'do workout'];
+
+//delete the 3rd item
+todos.splice(2,1);
+
+//add a new item at the end
+todos.push('last item');
+
+//remove the first item fro the list
+todos.shift();
+
+console.log(`You have ${todos.length} todos.`);
+console.log(todos);
+// console.log(`Todo: ${todos[0]}`);
+// console.log(`Todo: ${todos[todos.length - 2]}`);
+
+
+

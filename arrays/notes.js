4daee326759fd4442c8b64e2a3f4f00e6cf435de
Sandeep Sekhon <sandysss79@gmail.com>
2020-10-14 13:24:17 -0700

    modified notes.js and todo.js

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,12 +1,34 @@
-const notes = ['Note 1', 'Note 2', 'Note 3'];
-// console.log(notes.pop());
-// notes.push('Note 4');
+const notes = [ { 
+  title: 'My next trip',
+  body: 'I would like to go to Spain'
+}, 
+  { 
+     title: 'Habits to work on',
+     body: 'Exercise. Eating a bit better'
+  },
+  { 
+    title: 'Office modification',
+    body: 'Get a new seat'
+  }];
 
-// console.log(notes.shift());
-// notes.unshift('My first note');
+const findNote = function(notes, noteTitle) {
+  return notes.find(function(note, index) {
+    return note.title.toLowerCase() === noteTitle.toLowerCase();
+  });
+};
 
-//notes.splice(1, 1, 'This is the new second item' );
+// const findNote = function (notes, noteTitle) {
+//   const index = notes.findIndex(function (note, index) {
+//     return note.title.toLowerCase() === noteTitle.toLowerCase();
+//   });
+//   return notes[index];
+// }
 
-notes[2] = 'This is now the new note 3';
-console.log(notes.length);
-console.log(notes);
+const note = findNote(notes, 'office modification');
+console.log(note);
+
+
+// const index = notes.findIndex(function (note, index) {
+//   return note.title === 'Habits to work on';
+// });
+// console.log(index);
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,18 +1,53 @@
-const todos = ['study js', 'teach js', 'teach jus', 'do homework', 'do workout'];
+const todos = [{
+  text: 'study js',
+  completed: true
+},
+{
+  text: 'teach js',
+  completed: false
+},
+{
+  text: 'teach jus',
+  completed: true
+},
+{
+  text: 'do homework',
+  completed: false
+},
+{
+  text: 'do workout',
+  completed: true
+}];
 
-//delete the 3rd item
-todos.splice(2,1);
+const deleteTodo = function (todos, todoText) {
+  const index = todos.findIndex(function (todo) {
+    return todo.text.toLowerCase() === todoText.toLowerCase();
+  });
+  if(index > -1) {
+    todos.splice(index, 1);
+  }
+};
+deleteTodo(todos, 'do homework');
+console.log(todos);
 
-//add a new item at the end
-todos.push('last item');
+// //delete the 3rd item
+// todos.splice(2,1);
 
-//remove the first item fro the list
-todos.shift();
+// //add a new item at the end
+// todos.push('last item');
 
-console.log(`You have ${todos.length} todos.`);
-todos.forEach((todo, index) => {
-  console.log(`${index + 1}. ${todo}`);
-});
+// //remove the first item fro the list
+// todos.shift();
+
+// console.log(`You have ${todos.length} todos.`);
+
+// todos.forEach((todo, index) => {
+//   console.log(`${index + 1}. ${todo}`);
+// });
+
+// for(let i = 0; i < todos.length; i++) {
+//   console.log(`${i + 1} ${todos[i]}`);
+// }
 
 
 //console.log(todos);
